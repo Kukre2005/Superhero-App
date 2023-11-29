06a101d0107939cf105415ff0e6b17916d9f8c9f
kukre2005 <ariasmanuel191@gmail.com>
2023-11-29 13:37:52 -0300

    Superhero V1.1

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -68,6 +69,9 @@ fun HeroListItem(
     modifier: Modifier = Modifier
 ) {
     Card (
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+        modifier = modifier
+    ){
 
-    )
+    }
 }
